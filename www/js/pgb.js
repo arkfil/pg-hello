deef30f215f3b3d3cebb83796ee37201e4266c4c
Arkadiusz Filipowski <arek0051@outlook.com>
2018-03-12 15:36:57 +0100

    done

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -35,3 +35,16 @@ function checkConnection() {
 
     navigator.notification.alert('Connection type: ' + states[networkState]);
 }
+
+
+
+function onSuccess(acceleration) {
+    alert('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
+}
+
+function onError() {
+    alert('onError!');
+}
